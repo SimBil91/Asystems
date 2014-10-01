41c901e53f1ecea93f55e3ffe06e7d63d350393e
Simon Bilgeri <Simon.Bilgeri@tum.de>
2014-10-02 00:41:19 +0100

    Talker_Listener_Example

--- src/t_pkg/src/test.cpp
+++ src/t_pkg/src/test.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
 
-using namespace std;
+#include "ros/ros.h"
 
-int main() {
-	cout << "It works!\n";
-	cout << " Yes that's very nice!\n";
-	return 0;
+#include "std_msgs/String.h"
+
+#include <sstream>
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("Asystems", 1000);
+
+  ros::Rate loop_rate(2);
+  int count = 0;
+  while (ros::ok())
+  {
+
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "I run in 2Hz and I like to publish stuff. Count is:" << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+
+  return 0;
 }
